1ed835a61a3a2b9cc6b8e93cf048fa265b445cc9
link375 <https://github.com/link375>
2018-04-07 13:38:05 -0600

    create a visual of the die roll frequency

--- die_visual.py
+++ die_visual.py
@@ -1,3 +1,4 @@
+import pygal
 from die import Die
 
 # Create a D6
@@ -17,5 +18,16 @@ for value in range(1, die.num_sides+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
-print(results)
-print(frequencies)
+#print(results)
+#print(frequencies)
+
+# Visualize the results
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add("D6", frequencies)
+hist.render_to_file('die_visual.svg')
