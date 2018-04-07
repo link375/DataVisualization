3ecb5c1f20b63fea2b4d483384c4707a8bafb66a
link375 <17466655+link375@users.noreply.github.com>
2018-04-07 11:26:35 -0600

    line color

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,6 +1,13 @@
 import matplotlib.pyplot as plt
 
-plt.scatter(2, 4, s=200)
+#x_values = [1, 2, 3, 4, 5]
+#y_values = [1, 4, 9, 16, 25]
+
+x_values = list(range(1, 1001))
+y_values = [x**2 for x in x_values]
+
+
+plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolors='none', s=40)
 plt.show()
 
 # Set chart title and label axes
@@ -8,5 +15,10 @@ plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value", fontsize=14)
 plt.ylabel("Square of Value", fontsize=14)
 
+# Set the range for each axis
+plt.axis([0, 1100, 0, 1100000])
+
+
 # sEt size of tick labels
 plt.tick_params(axis='both', which='major', labelsize=14)
+
