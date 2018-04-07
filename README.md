b86ab5b792fd5ce2aca8c0027ab6791090f3015c
link375 <17466655+link375@users.noreply.github.com>
2018-04-07 11:12:42 -0600

    adding a single point to the screen

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DataVisualization
+
+Python 3.6.x
+
+pip install matplotlib
+
+pip install python3-tkinter
--- mpl_squares.py
+++ mpl_squares.py
@@ -1,7 +1,12 @@
 import matplotlib.pyplot as plt
 
+# x value
+input_values = [1, 2, 3, 4, 5]
+# y value
 squares = [1, 4, 9, 16, 25]
-plt.plot(squares, linewidth=5)
+
+# X / Y coordinates
+plt.plot(input_values, squares, linewidth=5)
 
 # Set chart title and labesl axes.
 plt.title("Square Numbers", fontsize=24)
@@ -11,5 +16,4 @@ plt.ylabel("Square of Value", fontsize=14)
 # Set size of tick labels
 plt.tick_params(axis='both', labelsize=14)
 
-plt.plot(squares)
 plt.show()
