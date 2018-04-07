94b5cabe64c4494a83ced8ce8b6a3256f0ed777a
link375 <https://github.com/link375>
2018-04-07 11:39:56 -0600

    save your plot as a png image

--- scatter_squares.py
+++ scatter_squares.py
@@ -6,8 +6,14 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x**2 for x in x_values]
 
+# colormap
+
+# plt.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, edgecolor='none', s=40)
+
 
 plt.scatter(x_values, y_values, c=(0, 0, 0.8), edgecolors='none', s=40)
+# save an image of your plot
+plt.savefig('squares_plot.png', bbox_inches='tight')
 plt.show()
 
 # Set chart title and label axes
