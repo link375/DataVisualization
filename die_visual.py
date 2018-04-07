056fd641e1d3065c4010d156e130ab754ba4a252
link375 <https://github.com/link375>
2018-04-07 13:48:57 -0600

    getting the frequency of a D6 and D10 being rolled at the same time

--- die_visual.py
+++ die_visual.py
@@ -3,12 +3,12 @@ from die import Die
 
 # Create a D6
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 # Make some rolls, and store results in a list
 results = []
 
-for roll_num in range(1000):
+for roll_num in range(50000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -28,9 +28,9 @@ hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 1000 times."
 # hist.x_labels = ['1', '2', '3', '4', '5', '6']
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12']
+hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
-hist.add('D6 + D6', frequencies)
+hist.add('D6 + D10', frequencies)
 hist.render_to_file('die_visual.svg')
