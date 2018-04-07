bd76c6345cfaaf6fae2707cbdbed5ce4188cd071
link375 <https://github.com/link375>
2018-04-07 12:55:24 -0600

    setting the size of the plotting window

--- rw_visual.py
+++ rw_visual.py
@@ -10,6 +10,9 @@ while True:
     rw.fill_walk()
     # plt.scatter(rw.x_values, rw.y_values, s=15)
 
+    # Set the size of the plotting window
+    plt.figure(figsize=(10, 6))
+
     # Plot the points, and show the plot
     point_numbers = list(range(rw.num_points))
     plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues,
