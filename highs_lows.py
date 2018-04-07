f642a5f24c140bebf8489b5df944011c22e3f1ab
link375 <https://github.com/link375>
2018-04-07 15:59:12 -0600

    plot all of the high temps

--- highs_lows.py
+++ highs_lows.py
@@ -1,11 +1,33 @@
 import csv
+from matplotlib import pyplot as plt
 
 filename = 'sitka_weather_07-2014.csv'
 
+# Get high temperatures from file
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
+    #print(header_row)
 
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
+    #for index, column_header in enumerate(header_row):
+    #    print(index, column_header)
+
+    highs = []
+    for row in reader:
+        high = int(row[1])
+        highs.append(high)
+
+    print(highs)
+
+# Plot data
+
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(highs, c='red')
+
+# Format plot
+plt.title("Daily high temperatures, July 2014", fontsize=14)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Temperature (F)", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+
+plt.show()
