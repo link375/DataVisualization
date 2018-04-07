63d64208343ea47f8a403a8ed0f4933133b6b274
link375 <https://github.com/link375>
2018-04-07 16:17:49 -0600

    plot highs to the dates on the csv file

--- highs_lows.py
+++ highs_lows.py
@@ -1,9 +1,10 @@
 import csv
 from matplotlib import pyplot as plt
+from datetime import datetime
 
 filename = 'sitka_weather_07-2014.csv'
 
-# Get high temperatures from file
+# Get dates and high temperatures from file
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
@@ -12,21 +13,28 @@ with open(filename) as f:
     #for index, column_header in enumerate(header_row):
     #    print(index, column_header)
 
-    highs = []
+    dates, highs = [], []
     for row in reader:
+        current_date = datetime.strptime(row[0], "%Y-%m-%d")
+        dates.append(current_date)
+
         high = int(row[1])
         highs.append(high)
 
     print(highs)
 
+    first_date = datetime.strptime('2014-7-1', '%Y-%m-%d')
+    print(first_date)
+
 # Plot data
 
 fig = plt.figure(dpi=128, figsize=(10, 6))
-plt.plot(highs, c='red')
+plt.plot(dates, highs, c='red')
 
 # Format plot
 plt.title("Daily high temperatures, July 2014", fontsize=14)
 plt.xlabel('', fontsize=16)
+fig.autofmt_xdate()
 plt.ylabel("Temperature (F)", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 
