74bad9d9e7641617c7c0fe4a3b9226a6791cf4a6
link375 <https://github.com/link375>
2018-04-07 14:31:41 -0600

    starting of parsing csv

--- die_visual.py
+++ die_visual.py
@@ -2,21 +2,22 @@ import pygal
 from die import Die
 
 # Create a D6
-die_1 = Die()
-die_2 = Die(10)
+die_1 = Die(6)
+die_2 = Die(6)
+die_3 = Die(6)
 
 # Make some rolls, and store results in a list
 results = []
 
-for roll_num in range(50000):
-    result = die_1.roll() + die_2.roll()
+for roll_num in range(1000):
+    result = die_1.roll() * die_2.roll() * die_3.roll()
     results.append(result)
 
 # analyze the results
 frequencies = []
-max_results = die_1.num_sides + die_2.num_sides
+max_results = die_1.num_sides + die_2.num_sides + die_3.num_sides
 
-for value in range(2, max_results+1):
+for value in range(3, max_results+1):
     frequency = results.count(value)
     frequencies.append(frequency)
 
@@ -28,7 +29,12 @@ hist = pygal.Bar()
 
 hist.title = "Results of rolling two D6 1000 times."
 # hist.x_labels = ['1', '2', '3', '4', '5', '6']
-hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
+# hist.x_labels = ['2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16']
+hist.x_labels = []
+for value in range(max_results-2):
+    hist.x_labels.append(value+3)
+
+#hist.x_labels = list(len(range(1, max_results)))
 hist.x_title = "Result"
 hist.y_title = "Frequency of Result"
 
