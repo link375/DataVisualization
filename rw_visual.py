f6df54014eb8d33524f27063b41d510668bd8795
link375 <https://github.com/link375>
2018-04-07 12:47:25 -0600

    remove axes so you can just the the visual walk

--- rw_visual.py
+++ rw_visual.py
@@ -20,6 +20,11 @@ while True:
     plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none',
                 s=100)
 
+    # Remove the axes
+    plt.axes().get_xaxis().set_visible(False)
+    plt.axes().get_yaxis().set_visible(False)
+
+
     plt.show()
 
     keep_running = input("Make another walk? (y/n): ")
