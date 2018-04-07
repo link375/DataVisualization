a447f398278a3a0540e3e4de3500fff2fb7f77d1
link375 <https://github.com/link375>
2018-04-07 11:44:59 -0600

    moving on

--- scatter_squares.py
+++ scatter_squares.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 #x_values = [1, 2, 3, 4, 5]
 #y_values = [1, 4, 9, 16, 25]
 
-x_values = list(range(1, 1001))
-y_values = [x**2 for x in x_values]
+x_values = list(range(1, 5001))
+y_values = [x**3 for x in x_values]
 
 # colormap
 
